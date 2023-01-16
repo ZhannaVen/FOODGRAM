27abd8f28e833a308664e26d88f13f7a8c2ad6e1
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-16 16:54:42 +0400

    admin.py in users has been changed

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,19 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-from .models import User
+User = get_user_model()
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'username',
-        'pk',
-        'email',
-        'role',
-        'bio',
-        'first_name',
-        'last_name')
-    list_editable = ('role',)
-    search_fields = ('username', 'email')
-    list_filter = ('role',)
+        'id', 'username', 'email',
+        'first_name', 'last_name',)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('email', 'first_name')
     empty_value_display = '-empty-'
