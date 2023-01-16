37f51088dac5e090683434f6ec493b3cb6cb0e85
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-16 16:47:58 +0400

    User Model has been changed

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,19 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.tokens import default_token_generator
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 from .validators import validate_username
 
 
 class User(AbstractUser):
     ADMIN = 'admin'
-    MODERATOR = 'moderator'
     USER = 'user'
     USER_ROLE_CHOICES = [
         (ADMIN, 'Administrator'),
-        (MODERATOR, 'Moderator'),
         (USER, 'User')
     ]
     username = models.CharField(
@@ -47,30 +42,12 @@ class User(AbstractUser):
         blank=True,
         verbose_name='Role of the user'
     )
-    bio = models.TextField(
-        blank=True,
-        verbose_name='Biography of the user'
-    )
-    confirmation_code = models.CharField(
-        max_length=250,
-        null=True,
-        blank=False,
-        default='00000',
-        verbose_name='Confirmation code for the password'
-    )
-
-    @property
-    def is_user(self):
-        return self.role == self.USER
-
-    @property
-    def is_admin(self):
-        return self.role == self.ADMIN
-
-    @property
-    def is_moderator(self):
-        return self.role == self.MODERATOR
-
+    REQUIRED_FIELDS = [
+        'username',
+        'first_name',
+        'last_name'
+    ]
+    
     class Meta:
         ordering = ('id',)
         verbose_name = 'User'
@@ -78,16 +55,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-
-@receiver(post_save, sender=User)
-def post_save(sender, instance, created, **kwargs):
-    if created:
-        confirmation_code = default_token_generator.make_token(
-            instance
-        )
-        instance.confirmation_code = confirmation_code
-        instance.save()
-
-
-
