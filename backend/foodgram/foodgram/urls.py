25e3dbd2ff6f20ea70f4c294035e3d278808c643
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-16 19:19:37 +0400

    added urls

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
+    path('api/', include('users.urls')),
 ]
