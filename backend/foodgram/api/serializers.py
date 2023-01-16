1f5074ed27118c1e1efba07cb44fad9fc165ffff
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-16 18:31:33 +0400

    all migrations were deleted

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,15 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.db import transaction
-from django.db.models import F
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
-from drf_extra_fields.fields import Base64ImageField
-from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag, Follow
-from rest_framework import status
-from rest_framework.exceptions import ValidationError
-from rest_framework.fields import IntegerField, SerializerMethodField
-from rest_framework.relations import PrimaryKeyRelatedField
-from rest_framework.serializers import ModelSerializer
+from recipes.models import Follow
+from rest_framework.fields import SerializerMethodField
+
 
 User = get_user_model()
 
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from djoser.views import UserViewSet
+from users.models import User
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+class CustomUserViewSet(UserViewSet):
+    '''Getting data about users.
+    '''
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
