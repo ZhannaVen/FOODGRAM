102706a2ec317de8c1520796a444b6b662aeb997
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-20 20:24:45 +0400

    page_size in pagination.py added

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,9 +1,7 @@
-from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Ingredient, Recipe, Tag
 from users.models import User
-
 
 user = User
 
