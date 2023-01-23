12fea2b67821ca1187a09ab494c787d71234cf79
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-23 14:07:23 +0400

    admin.py in users was updated

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,14 +1,39 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'username', 'email',
-        'first_name', 'last_name',)
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'followers',
+        'following',
+        'number_of_recipes'
+
+    )
     search_fields = ('email', 'username', 'first_name', 'last_name')
     list_filter = ('email', 'first_name')
     empty_value_display = '-empty-'
+
+    def followers(self, obj):
+        """The function counts total number
+        of followers.
+        """
+        return obj.follower.count()
+
+    def following(self, obj):
+        """The function counts total number
+        of followings.
+        """
+        return obj.following.count()
+
+    def number_of_recipes(self, obj):
+        """The function counts total number
+        of recipes.
+        """
+        return obj.recipes.count()
