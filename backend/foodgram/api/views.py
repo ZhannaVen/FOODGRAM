031a84076236d0536a7254e23549681d8f6116f8
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-01-25 18:13:04 +0400

    isort

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Sum
 from django.shortcuts import get_object_or_404
+
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, SAFE_METHODS, IsAuthenticated
+from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 
-from rest_framework.exceptions import ValidationError
-
 from users.models import User
 
 
